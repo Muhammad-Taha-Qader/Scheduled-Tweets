628ae8bd8e9080c76cb828101f1c19983b710ae4
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-11-07 13:33:42 +0500

    use Rails helper for generating links instead of href

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,19 @@ Rails.application.routes.draw do
   get "/about", to: "about#index"
   # $NOTES$ OR works same : get "about", to: "about#index"
   #
+  # <a class="nav-link" href="/about">About</a>
+  # <%#
+  # $NOTES$ above can be written as: <%= link_to "About", about_path, class="nav-link"
+  #                                 <%= link_to "is content", <UR route name>_path, class="nav-link"
+  #   Benifit of that is if you chnage your url link ie /about to /about_us you don't need to chnage the lick text everywhere
+  # just mention:
+  #                get "/about-us", to: "about#index" as: :about   and u can still use    <%= link_to "About", about_path, class="nav-link"  browser will respond to /about-us
+  #    instead of: get "/about", to: "about#index"
+  #             if u do: get "/about-us", to: "about#index"  it will auto make a name/prefix as about_us
+  #
+  # u can run rails routes  to see all routes in ur app
+  # %>
+
   # Get /
   get "/", to: "main#index"
   # $NOTES$ OR
