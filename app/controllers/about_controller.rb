f6bfd78fcf92ea3c4f960848d7237be8a3b39e36
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-11-07 12:11:24 +0500

    init and about test route

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController <ApplicationController
+  # if following is empty: we are saying if it does any reding then go for tat but if not then go to defult rails view which is view/about/index.html.erb
+  def index
+  end
+end
