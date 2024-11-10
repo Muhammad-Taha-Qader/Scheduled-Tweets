a43a81e504691a0365c08e1690621c85bc18ee79
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-11-10 10:09:49 +0500

    Creating the User model
    $ rails generate model User email:string password_digest:string
    	$ rails db:migrate
    	Use migration

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController <ApplicationController
   def index
     # flash is from ApplicationController
-    flash[:notice] = "Logged in Successfully"  # and it store this message in flash hash object and it's GLOBALLY avliable in our application
-    flash[:alert] = "Invalid email or password"
+    # flash[:notice] = "Logged in Successfully"  # and it store this message in flash hash object and it's GLOBALLY avliable in our application
+    # flash[:alert] = "Invalid email or password"
     # flash[:alert].now = "Invalid email or password"   '.now' to only display on current request
     # No point of saving them if not printed on screen, so we will make a partial view _flash
   end
